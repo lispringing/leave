e1802e8a5a87df063ee36e7536bf31b0543b629b
lispringing <2638526782@qq.com>
2024-12-06 12:57:27 +0800

    publish

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+function getTime() {
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var hour = date.getHours();
+    var minute = date.getMinutes();
+    var second = date.getSeconds();
+    var element = document.getElementById('time');
+    element.innerHTML = '当前时间：' + year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second ;
+    }
+    setInterval(getTime, 1000);
